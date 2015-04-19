d37d1f17bf8889044b78384eb9b075e82b51763e
Clay Carpenter <claycarpenter@gmail.com>
2015-04-19 23:55:49 +0000

    Imports initial version of the templater.

---
+++ index.js
@@ -0,0 +1,57 @@
+// Metalsmith Jade Templater.
+
+var jade = require('jade'),
+    path = require('path'),
+    fs = require('fs'),
+    Q = require('q');
+
+function jadeTemplater () {
+    var fs_readFile = Q.denodeify(fs.readFile);
+    
+    return function (files, metalsmith, done) {
+        var jadeTemplatePromises = [];
+        
+        Object.keys(files).forEach(function (filename) {
+            var file = files[filename];
+            
+            if (!file.template) {
+                // No template file specified, do nothing.
+                return;
+            }
+            
+            // Resolve the relative template file path to an absolute path.
+            var templateFilePath = path.resolve(file.template);
+            
+            var templateReadPromise = fs_readFile(templateFilePath, 'utf8');
+            templateReadPromise
+                .then(function (fileBuffer) {
+                    var templateContents = fileBuffer.toString();
+                    
+                    var templateFn = jade.compile(templateContents, {pretty: true, filename: filename});
+                    var locals = Object.create(null);
+                    locals.data = file;
+                    var htmlOutput = templateFn(locals);
+                    
+                    file.contents = new Buffer(htmlOutput);
+                })
+                .catch(function (error) {
+                    // TODO: How should this error be handled?
+                    console.error(error.toString());
+                    
+                    throw error;
+                });
+            
+            jadeTemplatePromises.push(templateReadPromise);
+        });
+        
+        Q.all(jadeTemplatePromises)
+            .then(function (result) {
+                done();
+            })
+            .catch(function (error) {
+                console.error(error.toString());
+            });
+    };
+}
+
+module.exports = jadeTemplater;
